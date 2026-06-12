07a4f75dbf92143bd16fb3c0cddee71d282b3109
Iman <imanma1@outlook.com>
2026-06-12 00:40:49 -0400

    changed the bash file for HPC

--- simulations/submit_sim.sh
+++ simulations/submit_sim.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
-#SBATCH --account=def-zjunxi_cpu
+
 #SBATCH --job-name=conformal_sim
-#SBATCH --time=10:30:00
-#SBATCH --cpus-per-task=128
-#SBATCH --mem=512G
-#SBATCH --array=1-46:5
-#SBATCH --output=sim_log_%A_%a.txt
+
+#SBATCH --time=00:30:00
+
+#SBATCH --cpus-per-task=48
+
+#SBATCH --mem=192G
+
+#SBATCH --output=sim_log_%j.txt
+
+
 
 module load StdEnv/2023
+
 module load r/4.5.0
+
+
 
 cd ~/scratch/CAMS/simulations
 
-Rscript run_sim.R $SLURM_ARRAY_TASK_ID
+
+
+Rscript run_sim.R 1
